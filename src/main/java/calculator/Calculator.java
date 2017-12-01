93746cfc0dbe5945a2138b127e250e9fb9ce4896
Klaus Hetzelein <klaus.hetzelein@gmx.de>
2017-12-01 14:56:51 +0100

    war wieder auf jre1.5

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,13 +1,13 @@
 package calculator;
 
 public class Calculator implements ICalculator {
-    @Override
-	public int add(final int a, final int b) {
+    public int add(final int a, final int b)
+    {
 		return a + b;
 	}
 
-    @Override
-	public int sub(final int a, final int b) {
+    public int sub(final int a, final int b)
+    {
 		return a - b;
 	}
 
